5aa624e0e67d57b13b10d6affe4835b6725c7454
meyyappansv <meyysevugan@ehaven.com>
2025-04-28 23:05:07 -0400

    File scrubber initial commit

--- wopro_file_scrubber/Form1.Designer.cs
+++ wopro_file_scrubber/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            folderLocation = new TextBox();
+            scrubFiles = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(67, 118);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Result Files Folder";
+            // 
+            // folderLocation
+            // 
+            folderLocation.Location = new Point(193, 118);
+            folderLocation.Name = "folderLocation";
+            folderLocation.Size = new Size(153, 23);
+            folderLocation.TabIndex = 1;
+            // 
+            // scrubFiles
+            // 
+            scrubFiles.Location = new Point(122, 171);
+            scrubFiles.Name = "scrubFiles";
+            scrubFiles.Size = new Size(118, 23);
+            scrubFiles.TabIndex = 2;
+            scrubFiles.Text = "Scrub Files";
+            scrubFiles.UseVisualStyleBackColor = true;
+            scrubFiles.Click += scrubFiles_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(436, 283);
+            Controls.Add(scrubFiles);
+            Controls.Add(folderLocation);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "woPro File Scrubber";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox folderLocation;
+        private Button scrubFiles;
     }
 }
--- wopro_file_scrubber/Form1.cs
+++ wopro_file_scrubber/Form1.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace wopro_file_scrubber
 {
     public partial class Form1 : Form
@@ -5,6 +10,85 @@ namespace wopro_file_scrubber
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void scrubFiles_Click(object sender, EventArgs e)
+        {
+            bool flag1 = false;
+            string searchPattern1 = "*_cm.txt";
+            string searchPattern2 = "*_pt.txt";
+            string path = folderLocation.Text;
+            try
+            {
+                bool flag2 = false;
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                foreach (FileInfo file in directoryInfo.GetFiles(searchPattern1))
+                {
+                    string path1 = path + "\\" + file.Name;
+                    File.Copy(path1, path1+"_bkp", overwrite: true);
+                    processFile(path1);
+
+
+                }
+
+                foreach (FileInfo file in directoryInfo.GetFiles(searchPattern2))
+                {
+                    string path2 = path + "\\" + file.Name;
+                    File.Copy(path2, path2 + "_bkp", overwrite: true);
+                    processFile(path2);
+
+                }
+
+
+                MessageBox.Show("Scrubbing All File Complete");
+                folderLocation.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+            }
+           
+        }
+        private void processFile(string path)
+        {
+            
+            var lines = new List<string>(File.ReadAllLines(path));
+            var newLines = new List<string>();
+            string fileOrder = "";
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("Checklist Log for Work Order:") && line.Contains("File Order:")) //match condition
+                {
+                    // Update the matched line
+                    Match match = Regex.Match(line, @"File Order:\s*\d+");
+                    if (match.Success)
+                    {
+                        fileOrder = match.Value; // <-- the full matched text
+                       
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("No match found.");
+                    }
+                   
+                    string updatedLine = Regex.Replace(line, @"File Order:\s*\d+,", "");
+                    string normalized = Regex.Replace(updatedLine, @"\t+", "\t");
+                    newLines.Add(normalized);
+
+                    // Add a new line after it
+                    newLines.Add(": "+ fileOrder);
+                }
+                else
+                {
+                    newLines.Add(line);
+                }
+            }
+
+            File.WriteAllLines(path, newLines);
+
+            Console.WriteLine("Update complete!");
         }
     }
 }
